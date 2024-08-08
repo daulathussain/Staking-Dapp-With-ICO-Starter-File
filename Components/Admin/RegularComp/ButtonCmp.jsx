bfced991a7a9937e0ef62e367f8cfe0a8e320e7b
Daulat Hussain <102717032+daulathussain@users.noreply.github.com>
2024-08-08 11:27:36 +0530

    Update ButtonCmp.jsx

--- Components/Admin/RegularComp/ButtonCmp.jsx
+++ Components/Admin/RegularComp/ButtonCmp.jsx
@@ -1,21 +1,7 @@
 import React from "react";
 
-const ButtonCmp = ({ tab, name, styleClass }) => {
-  return (
-    <li className="nav-item" role="presentation">
-      <button
-        className={` ${styleClass}`}
-        data-bs-toggle="tab"
-        data-bs-target={`#tab-${tab}`}
-        type="button"
-        role="tab"
-        aria-controls={`tab-${tab}`}
-        aria-selected="true"
-      >
-        {name}
-      </button>
-    </li>
-  );
+const ButtonCmp = () => {
+  return <div>ButtonCmp</div>;
 };
 
 export default ButtonCmp;
