21a263f63bdd079980993d7d8bf326743ab20898
Daulat Hussain <102717032+daulathussain@users.noreply.github.com>
2024-08-08 11:28:52 +0530

    Update InputRatio.jsx

--- Components/Admin/RegularComp/InputRatio.jsx
+++ Components/Admin/RegularComp/InputRatio.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 
-const InputRatio = ({ value, index }) => {
-  return (
-    <li>
-      <input id={`type${index}`} type="radio" name="type" />
-      <label htmlFor={`type${index}`}>{value}</label>
-    </li>
-  );
+const InputRatio = () => {
+  return <div>InputRatio</div>;
 };
 
 export default InputRatio;
