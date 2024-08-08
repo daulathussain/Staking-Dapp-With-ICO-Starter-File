b22e9e56a7079d4ae4c6d9d00cb38836c5c6f263
Daulat Hussain <102717032+daulathussain@users.noreply.github.com>
2024-08-08 11:27:57 +0530

    Update ClickButton.jsx

--- Components/Admin/RegularComp/ClickButton.jsx
+++ Components/Admin/RegularComp/ClickButton.jsx
@@ -1,17 +1,7 @@
 import React from "react";
 
-const ClickButton = ({ name, handleClick }) => {
-  return (
-    <div className="col-12">
-      <button
-        type="button"
-        className="form__btn form__btn--small"
-        onClick={handleClick}
-      >
-        {name}
-      </button>
-    </div>
-  );
+const ClickButton = () => {
+  return <div>ClickButton</div>;
 };
 
 export default ClickButton;
